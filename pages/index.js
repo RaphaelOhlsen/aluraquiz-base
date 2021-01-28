e474c09e478a3c9222d4e0d9f799e9753a23fb5d
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2021-01-28 18:38:46 -0300

    Fim da aulda 2

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
+import React, { useState } from 'react';
+import Head from 'next/head';
 import styled from 'styled-components';
+import { useRouter } from 'next/router';
+
 import db from '../db.json';
+
 import Widget from '../src/components/Widget';
-import QuizBackground from '../src/components/QuizBackground'
+import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
+import QuizLogo from '../src/components/QuizLogo';
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -17,16 +23,41 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const [name, setNane] = useState('');
+  const router = useRouter();
+
+  function handleOnSubmit(ev) {
+    ev.preventDefault();
+    router.push(`/qiz?Name=${name}`);
+  }
+
+  function handleOnChange(ev) {
+    const newName = ev.target.value;
+    setNane(newName);
+  }
+
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>AluraQuiz - Modelo Base</title>
+      </Head>
       <QuizContainer>
-
+        <QuizLogo />
         <Widget>
           <Widget.Header>
             <h1>The legend of zelda</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>Lorem ipsum dolor sit amet consectetur adiHic op</p>
+            <form onSubmit={(ev) => handleOnSubmit(ev)}>
+              <input
+                placeholder="Digite o seu nome"
+                value={name}
+                onChange={(ev) => handleOnChange(ev)}
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar  {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
 
@@ -40,7 +71,7 @@ export default function Home() {
         </Widget>
         <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/omariosouto"/>
+      <GitHubCorner projectUrl="https://github.com/omariosouto" />
     </QuizBackground>
   );
 }
